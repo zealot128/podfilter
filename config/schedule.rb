c5f846004d7bbe033d91e0d434c283e80791531e
Stefan Wienert <stwienert@gmail.com>
2014-03-08 09:49:00 +0100

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ job_type :runner, "cd :path && bin/rails runner -e :environment ':task' :output"
 job_type :checker,"pgrep -f -u stefan sidekiq || (cd :path && :environment_variable=:environment bundle exec rake :task --silent :output)"
 
 every 10.minutes do
-  command "pgrep -f -u stefan 'sidekiq #{Sidekiq::VERSION} prod' || (rm -f /apps/podfilter/prod/current/log/sidekiq.log && cd /apps/podfilter/prod/current && ~/.rvm/bin/rvm 2.1.1 do bundle exec sidekiq -d -i 0 -P /apps/podfilter/prod/current/tmp/pids/sidekiq.pid -e production -L /apps/podfilter/prod/current/log/sidekiq.log)"
+  command "pgrep -f -u stefan 'sidekiq 2.7' || (rm -f /apps/podfilter/prod/current/log/sidekiq.log && cd /apps/podfilter/prod/current && ~/.rvm/bin/rvm 2.1.1 do bundle exec sidekiq -d -i 0 -P /apps/podfilter/prod/current/tmp/pids/sidekiq.pid -e production -L /apps/podfilter/prod/current/log/sidekiq.log)"
 end
 
 every 7.days, at: '03:35' do
