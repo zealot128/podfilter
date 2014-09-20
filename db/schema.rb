bafc0cbc9bb1a22590a27ba5ab62688099780aa3
Stefan Wienert <stefan.wienert@pludoni.de>
2014-09-20 21:47:04 +0200

    db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920175957) do
+ActiveRecord::Schema.define(version: 20140920194620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20140920175957) do
   enable_extension "fuzzystrmatch"
 
   create_table "categories", force: true do |t|
-    t.string   "name"
-    t.string   "keywords"
+    t.string   "name",       limit: 255
+    t.string   "keywords",   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -33,11 +33,11 @@ ActiveRecord::Schema.define(version: 20140920175957) do
   add_index "categories_podcasts", ["category_id", "podcast_id"], name: "categories_podcasts_index", unique: true, using: :btree
 
   create_table "change_requests", force: true do |t|
-    t.string   "type"
+    t.string   "type",       limit: 255
     t.boolean  "completed"
     t.integer  "owner_id"
     t.hstore   "payload"
-    t.string   "token"
+    t.string   "token",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -48,19 +48,19 @@ ActiveRecord::Schema.define(version: 20140920175957) do
   add_index "change_requests", ["type"], name: "index_change_requests_on_type", using: :btree
 
   create_table "duplicate_candidates", force: true do |t|
-    t.integer "ids", array: true
-    t.string  "md5"
+    t.integer "ids",             array: true
+    t.string  "md5", limit: 255
   end
 
   add_index "duplicate_candidates", ["md5"], name: "index_duplicate_candidates_on_md5", unique: true, using: :btree
 
   create_table "episodes", force: true do |t|
-    t.string   "title"
+    t.string   "title",       limit: 255
     t.text     "description"
     t.text     "url"
-    t.string   "image"
+    t.string   "image",       limit: 255
     t.datetime "pubdate"
-    t.string   "guid"
+    t.string   "guid",        limit: 255
     t.integer  "source_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -77,11 +77,11 @@ ActiveRecord::Schema.define(version: 20140920175957) do
 
   create_table "identities", force: true do |t|
     t.integer  "owner_id"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "email"
-    t.string   "name"
-    t.string   "image"
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
+    t.string   "email",      limit: 255
+    t.string   "name",       limit: 255
+    t.string   "image",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -93,9 +93,9 @@ ActiveRecord::Schema.define(version: 20140920175957) do
     t.integer  "owner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "md5"
-    t.string   "name"
-    t.string   "type"
+    t.string   "md5",        limit: 255
+    t.string   "name",       limit: 255
+    t.string   "type",       limit: 255
   end
 
   add_index "opml_files", ["md5", "owner_id"], name: "index_opml_files_on_md5_and_owner_id", using: :btree
@@ -111,10 +111,10 @@ ActiveRecord::Schema.define(version: 20140920175957) do
   add_index "opml_files_sources", ["source_id"], name: "index_opml_files_sources_on_source_id", using: :btree
 
   create_table "owners", force: true do |t|
-    t.string   "token"
+    t.string   "token",               limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
+    t.string   "image",               limit: 255
     t.boolean  "admin"
     t.integer  "primary_identity_id"
   end
@@ -125,11 +125,11 @@ ActiveRecord::Schema.define(version: 20140920175957) do
   create_table "podcasts", force: true do |t|
     t.text     "title"
     t.text     "description"
-    t.string   "image"
+    t.string   "image",            limit: 255
     t.integer  "subscriber_count"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "language"
+    t.string   "language",         limit: 255
   end
 
   create_table "recommendations", force: true do |t|
@@ -142,15 +142,15 @@ ActiveRecord::Schema.define(version: 20140920175957) do
   add_index "recommendations", ["weight"], name: "index_recommendations_on_weight", using: :btree
 
   create_table "sources", force: true do |t|
-    t.string   "url"
-    t.string   "title"
+    t.string   "url",          limit: 255
+    t.string   "title",        limit: 255
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
+    t.string   "image",        limit: 255
     t.boolean  "offline"
     t.boolean  "active"
-    t.string   "ancestry"
+    t.string   "ancestry",     limit: 255
     t.integer  "owners_count"
     t.integer  "podcast_id"
     t.boolean  "has_media"
@@ -168,17 +168,19 @@ ActiveRecord::Schema.define(version: 20140920175957) do
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
-    t.string   "taggable_type"
+    t.string   "taggable_type", limit: 255
     t.integer  "tagger_id"
-    t.string   "tagger_type"
+    t.string   "tagger_type",   limit: 255
     t.string   "context",       limit: 128
     t.datetime "created_at"
   end
 
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: true do |t|
-    t.string "name"
+    t.string  "name",           limit: 255
+    t.integer "taggings_count",             default: 0
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
