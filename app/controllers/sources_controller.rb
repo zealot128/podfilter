0598a4586d9cd1ea309846077543bc4959f5da8e
Stefan Wienert <stefan.wienert@pludoni.de>
2015-06-20 15:22:50 +0200

    legacy redirects do not crash

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -2,10 +2,11 @@ class SourcesController < ApplicationController
   load_and_authorize_resource
 
   def show
-    @podcast = @source.podcast
     if @source.redirected_to
-      redirect_to [@podcast, @source.redirected_to]
+      redirect_to [@source.redirected_to.podcast, @source.redirected_to]
+      return
     end
+    @podcast = @source.podcast
     render 'podcasts/show'
   end
 
