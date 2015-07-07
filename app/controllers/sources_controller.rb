c9c86b91897658958aa730334213ec217bce3435
Stefan Wienert <stefan.wienert@pludoni.de>
2015-07-07 09:00:18 +0200

    Source show: alle episodes koennen in 100er Schritten seitenweise durchgegangen werden

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -6,6 +6,8 @@ class SourcesController < ApplicationController
       redirect_to [@source.redirected_to.podcast, @source.redirected_to]
       return
     end
+    @page = [1, params[:page].to_i].max
+    @episodes = @source.episodes.order('pubdate desc').page(@page).per(100)
     @podcast = @source.podcast
     if @podcast.nil?
       render 'show'
