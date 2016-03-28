70eed26cdc75db8c6743e3c44ef408a5579d325d
Stefan Wienert <stefan.wienert@pludoni.de>
2016-03-28 11:52:17 -0400

    Maybe fix: Redis crash

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 if !Rails.env.test?
   require 'exception_notification/sidekiq'
   Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_podfilter_#{Rails.env}" }
+    config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_podfilter_#{Rails.env}" }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_podfilter_#{Rails.env}" }
+    config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_podfilter_#{Rails.env}" }
   end
 end
